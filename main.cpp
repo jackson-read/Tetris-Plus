ccdf1424633d03e30e8a72d87654b4984dcb5bbc
Jackson <jackson@readbch.com>
2026-03-28 13:27:30 -0400

    Adding basic main function

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-//This is our main 
+// main.cpp
+// This is our main where everything will happen. We will call the other files.
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Tetris Plus starting..." << std::endl;
+    // TODO: initialize game, load resources, enter game loop
+    return 0;
+}
