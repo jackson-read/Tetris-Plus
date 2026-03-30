951b9ecbaaa327b346e220e2c4f1d71fe45345f3
YamiGJHL <gabrielluis1741@gmail.com>
2026-03-30 17:22:30 -0400

    Added grid and baseline loop to check boolean values

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
-//This is our main 
+#include <iostream>
+#include <vector>
+
+
+void Engine() {
+	int rows = 20;
+	int columns = 10;
+	std::vector<std::vector<bool>> grid(rows, std::vector<bool>(columns, 0)); // Array grid 
+	
+	for (int i = 0; i < 20; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (grid[i][j] != 0) {
+				// add code with Blocks class to not move them after check
+			}
+		}
+	}
+}
+
+
+
+int main() {
+
+}
