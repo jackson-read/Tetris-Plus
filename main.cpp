38cda6443e1e665af93726d80ab05e1fdde9ea97
YamiGJHL <gabrielluis1741@gmail.com>
2026-04-03 11:27:31 -0400

    added block spawning function

--- main.cpp
+++ main.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <vector>
-
+#include "blocks.h"
+#include <cstdlib>   
+#include <ctime>
 
 class GameEngine {
 private:
     
+    Block* activeBlock;
     int score;
     const int rows = 20;
     const int columns = 10;
-
    
     std::vector<std::vector<int>> grid;
-
-    
 
 public:
     GameEngine() {
@@ -50,12 +50,33 @@ public:
     
     int getScore() const { return score; }
     std::vector<std::vector<int>> getGrid() const { return grid; }
+
+    void SpawnNextBlock(){
+        
+        int randomBlock = rand() % 7;
+        int startX = 3;
+        int startY = 2;
+
+        switch (randomBlock) {
+        case 0: activeBlock = new TBlock(startX, startY); break;
+        case 1: activeBlock = new OBlock(startX, startY); break;
+        case 2: activeBlock = new IBlock(startX, startY); break;
+        case 3: activeBlock = new LBlock(startX, startY); break;
+        case 4: activeBlock = new JBlock(startX, startY); break;
+        case 5: activeBlock = new ZBlock(startX, startY); break;
+        case 6: activeBlock = new SBlock(startX, startY); break;
+        }
+    }
 };
 
 int main() {
+    srand(time(0));
+
     GameEngine engine;
 
-   
+    int e = rand() % 100;
+    std::cout << e;
+
     std::cout << "Engine initialized. Current Score: " << engine.getScore() << std::endl;
 
     return 0;
